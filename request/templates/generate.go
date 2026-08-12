16e546e540757d7e7321bf88057ed2d161236baa
Rob Stradling <rob@sectigo.com>
2026-08-12 17:38:11 +0100

    build: keep quicktemplate/qtc dep via blank import
    Blank-import qtc so `go mod tidy` doesn't drop the dependency used by the
    //go:generate directive.

--- request/templates/generate.go
+++ request/templates/generate.go
@@ -1,3 +1,5 @@
 package templates
 
+import _ "github.com/valyala/quicktemplate/qtc" // prevent "go tidy" from removing the dependency
+
 //go:generate go run github.com/valyala/quicktemplate/qtc@latest -dir=.
