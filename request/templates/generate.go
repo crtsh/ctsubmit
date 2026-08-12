fd3a2d9cf4c4c840d0c35515b9d5a95fc399dbc6
Rob Stradling <rob@sectigo.com>
2026-08-12 17:44:50 +0100

    fix import

--- request/templates/generate.go
+++ request/templates/generate.go
@@ -1,5 +1,5 @@
 package templates
 
-import _ "github.com/valyala/quicktemplate/qtc" // prevent "go tidy" from removing the dependency
+import _ "github.com/valyala/quicktemplate" // prevent "go tidy" from removing the dependency
 
 //go:generate go run github.com/valyala/quicktemplate/qtc@latest -dir=.
