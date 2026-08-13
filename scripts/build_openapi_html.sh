4656217c51f203401c89b8205bbd30d1db4c5921
Rob Stradling <rob@sectigo.com>
2026-08-13 16:24:18 +0100

    Pin @redocly/cli to 2.46.1 in build_openapi_html.sh and package.json (drop @latest)

--- scripts/build_openapi_html.sh
+++ scripts/build_openapi_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "$0")")"
-npm list @redocly/cli || npm i @redocly/cli@latest
-npx @redocly/cli@latest build-docs ../docs/openapi.yaml -o ../docs/openapi.html
+npm list @redocly/cli || npm i @redocly/cli@2.46.1
+npx @redocly/cli@2.46.1 build-docs ../docs/openapi.yaml -o ../docs/openapi.html
